83c9e9d64893f6805d488a46f2ed9c09ad1730b5
JH <140699318+Jilani-H@users.noreply.github.com>
2023-12-15 01:21:18 +0600

    Changed static data with real API

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,10 +1,32 @@
+"use client";
 import Image from "next/image";
+import AnimeCard, { AnimeProp } from "./AnimeCard";
+import { useEffect, useState } from "react";
+
+import { InView, useInView } from "react-intersection-observer";
+import { fetchAnime } from "@/app/action";
 
 function LoadMore() {
+  const { ref, inView } = useInView();
+  const [data, setData] = useState<AnimeProp[]>([]);
+
+  useEffect(() => {
+    if (inView) {
+      fetchAnime(2).then((res: any) => {
+        setData([...data, ...res]);
+      });
+    }
+  }, [inView, data]);
+
   return (
     <>
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data.map((item: AnimeProp, index: number) => (
+          <AnimeCard key={item.id} anime={item} index={index} />
+        ))}
+      </section>
       <section className="flex justify-center items-center w-full">
-        <div>
+        <div ref={ref}>
           <Image
             src="./spinner.svg"
             alt="spinner"
